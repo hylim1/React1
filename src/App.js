409765c93ea0e8a77570a093c0759b16cc3dcbd7
hylim1 <93750714+hylim1@users.noreply.github.com>
2022-05-05 08:46:36 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card } from "react-bootstrap";
+
+function CatCard(props) {
+  return (
+    <div style={{ width: "18rem", border: "2px solid black" }}>
+      <img src= {props.cat.image} width={150} height={150} />
+      <div>
+        <div>{props.cat.name}</div>
+        <div>{props.cat.description}</div>
+      </div>
+    </div>
+  );
+}
+
+const catData = [
+  {
+    name: "Abyssinian",
+    image: "https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg",
+    description:
+      "The Abyssinian is easy to care for, and a joy to have in your home. They're affectionate cats and love both people and other animals.",
+  },
+  {
+    name: "Aegean",
+    image: "https://cdn2.thecatapi.com/images/ozEvzdVM-.jpg",
+    description:
+      "Native to the Greek islands known as the Cyclades in the Aegean Sea, these are natural cats, meaning they developed without humans getting involved in their breeding.",
+  },
+];
+
+function CatFeed() {
+  return (
+    <div>
+      {
+        catData.map((link)=> {
+          return <CatCard cat = {link}/>;})
+      }
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CatFeed />
     </div>
   );
 }
